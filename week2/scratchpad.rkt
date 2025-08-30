fc2b2e24dd8fde764f71cdcca380c574bf3b42b3
james <dead.end@noreply.com>
2025-08-30 13:26:47 -0500

    finished assignment 1

--- week2/scratchpad.rkt
+++ week2/scratchpad.rkt
@@ -18,44 +18,170 @@
   (cases tat x
     {null-val (val) #t}
     {node-val (a l r) #f}
-  )
-)
+))
 
-;returns 0 if no children or empty
-;1 for left child
-;2 for right child
-;3 for both children
-(define (children? x)
-  (cases tat x
-    {null-val (val) 0}
+(define (insert t at)
+  (cases tat t
+    {null-val (val) 
+      (node-val at (null-val '()) (null-val '()))}
+    {node-val (a l r) 
+      (cases atom a
+        {atom-val (a-val)
+          (cases atom at
+            {atom-val (at-val)
+              (if (< at-val a-val) 
+                (node-val a (insert l at) r) ;;left child
+                (node-val a l (insert r at)) ;;right child
+          )})
+      })
+}))
+
+(define (member? t at)
+  (cases tat t
+    {null-val (val) #f}
     {node-val (a l r)
-      (cond 
-        [(and (not(empty? l)) (not(empty? r))) 3] ;;both full
-        [(and (empty? l) (empty? r)) 0] ;;both empty
-        [(empty? l) 2] ;;left empty so right child
-        [else 1]       ;;left was full so left child
-      )
+      (cases atom a
+        {atom-val (a-val)
+          (cases atom at
+            {atom-val (at-val)
+              (if (= a-val at-val) 
+                #t ;;found it
+                (if (< at-val a-val) 
+                  (member? l at) ;;left child
+                  (member? r at) ;;right child
+          ))})
+      })
+}))
 
-    }
-  )
+
+(define (appendTat t apende)
+  (cases tat t
+    {null-val (_) apende} ;;return the appende
+    {node-val (a l r) 
+      (cases atom a
+        {atom-val (a-val)
+          (cases tat apende
+            {null-val (_) t} ;;return the original
+            {node-val (ap-at ll rr) 
+              (cases atom ap-at
+                {atom-val (ap-val)
+                  (if (< ap-val a-val);; do we go left or right
+                    (appendTat apende t) ;;swap whos being inserted
+                    (node-val a (appendTat l apende) r) ;;insert left
+              )})
+            }
+          )})
+      })
 )
 
 
-(define a (atom-val "A"))
-(define b (atom-val "B"))
+(define (delete t at)
+  (cases tat t
+    {null-val (val) #f}
+    {node-val (a l r)
+      (cases atom a
+        {atom-val (a-val)
+          (cases atom at
+            {atom-val (at-val)
+              (if (= a-val at-val) 
+                ;;to remove the current node we add the right child 
+                ;;to the left child and return the new sub tree
+                (appendTat l r)
+                (if (< at-val a-val) 
+                  (node-val a (delete l at) r) ;;left child
+                  (node-val a l (delete r at)) ;;right child
+          ))})
+      })
+}))
 
-(define c (atom-val "C"))
-(define r (atom-val "R"))
-(define l (atom-val "L"))
+(define (print-atom node)
+  (cases tat node
+    {null-val (val) (displayln "()")}
+    {node-val (a l r) 
+      (displayln a)}
+  )
+)
+
+(define (atom++ node)
+  (cases tat node
+    {null-val (val) node}
+    {node-val (a l r) 
+      (cases atom a
+        {atom-val (a-val)
+          (node-val (atom-val (+ a-val 1)) l r)})}
+  )
+)
+
+(define (pre-order t ex)
+  (begin 
+    (define new-t (ex t))
+    (when (tat? new-t) ; update the node
+      (set! t new-t))
+    (cases tat t
+      {null-val (val) t}
+      {node-val (a l r) 
+        (node-val a (pre-order l ex) (pre-order r ex))
+    })
+  )
+)
+
+(define (in-order t ex)
+    (cases tat t
+      {null-val (val) t}
+      {node-val (a l r) 
+        (begin
+          (define newleft (pre-order l ex)) ;;travers left side
+          (define new-t (ex (node-val a newleft r))) ;work current node
+          (when (tat? new-t) ; update the node
+            (set! t new-t))
+          (cases tat t 
+            {null-val (val) t}
+            {node-val (a l r) 
+              (node-val a l (in-order r ex)) ;;set final and traverse right
+          })
+        )
+      }
+))
+
+(define (post-order t ex)
+  (cases tat t
+      {null-val (val) t}
+      {node-val (a l r) 
+        (begin
+          (define newleft (post-order l ex)) ;;travers left side
+          (define newRight (post-order r ex)) ;;travers right side
+          (define new-t (ex (node-val a newleft newRight))) ;work current node
+          (when (tat? new-t) ; update the node
+            (set! t new-t))
+          (cases tat t 
+            {null-val (val) t}
+            {node-val (a l r) t} ;;return the node
+          )
+        )
+      }
+  )
+)
+
+(define a (atom-val 1))
+(define b (atom-val 2))
+
+(define c (atom-val 3))
+(define r (atom-val 18))
+(define l (atom-val 12))
 
 (define empty (null-val '()))
-(define baren (node-val a empty empty))
+(define baren (node-val (atom-val 0) empty empty))
 (define left (node-val l baren empty))
 (define right (node-val r empty baren))
 (define alphabet (node-val a {node-val b empty empty} {node-val c empty empty}))
+(define extraAlpha (insert (insert alphabet r) l))
 
-(displayln (children? empty))
-(displayln (children? baren))
-(displayln (children? left))
-(displayln (children? right))
-(displayln (children? alphabet))
+
+(displayln (insert baren c))
+(displayln (member? alphabet c))
+(displayln (member? baren c))
+(displayln (appendTat alphabet baren))
+(displayln (pre-order alphabet print-atom))
+(displayln (pre-order alphabet atom++))
+(in-order alphabet print-atom)
+(post-order alphabet print-atom)
