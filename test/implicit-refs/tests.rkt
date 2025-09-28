a775d6eb3b83a8e5c20000b6d45d9dc8a4764eb6
james <dead.end@noreply.com>
2025-09-28 10:27:24 -0500

    done testing

---
+++ test/implicit-refs/tests.rkt
@@ -0,0 +1,42 @@
+#lang eopl
+(require eopl/tests/private/utils)
+
+(require "data-structures.rkt")  ; for expval constructors
+(require "lang.rkt")             ; for scan&parse
+(require "interp.rkt")           ; for value-of-program
+(require racket/base)
+
+;; run : String -> ExpVal
+;; Page: 71
+(define run
+  (lambda (string)
+    (value-of-program (scan&parse string))))
+
+(define equal-answer?
+  (lambda (ans correct-ans)
+    (equal? ans (sloppy->expval correct-ans))))
+
+(define sloppy->expval 
+  (lambda (sloppy-val)
+    (cond
+      ((number? sloppy-val) (num-val sloppy-val))
+      ((boolean? sloppy-val) (bool-val sloppy-val))
+      (else
+       (eopl:error 'sloppy->expval 
+                   "Can't convert sloppy value to expval: ~s"
+                   sloppy-val)))))
+
+(define-syntax-rule (check-run (name str res) ...)
+  (begin
+    (cond [(eqv? 'res 'error)
+           (check-exn always? (lambda () (run str)))]
+          [else
+           (check equal-answer? (run str) 'res (symbol->string 'name))])
+    ...))
+
+;;;;;;;;;;;;;;;; tests ;;;;;;;;;;;;;;;;
+
+(println (run "greater?(1,0)"))
+(println (run "greater?(0,0)"))
+(println (run "greater?(0,1)"))
+(println (run  "let y = 3 in while (greater?(y,0)) set y =-(y,1)"))
